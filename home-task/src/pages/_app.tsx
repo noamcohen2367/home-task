bfeaca8381ed8a214760420432e63e4b6a607114
noamcohen2367 <134809099+noamcohen2367@users.noreply.github.com>
2025-05-26 20:41:46 +0300

    Delete home-task directory

--- home-task/src/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import '@/styles/globals.css';
-import '@/styles/landing-page.css';
-import LandingPageMain from './LandingPageMain';
-export default function App() {
-  return <LandingPageMain />;
-}
